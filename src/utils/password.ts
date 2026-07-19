4f533433fbe8dcebe6ca267871c94eb95985c7ae
Santyyy <129233495+EseSx@users.noreply.github.com>
2026-07-19 01:18:58 -0300

    style: formated document

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,15 +1,18 @@
 // Importa bcrypt para el hashing de contraseñas
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 // Define el número de rondas de sal para el hashing (Ni muy bajo ni muy alto, un valor de 12 es un buen equilibrio entre seguridad y rendimiento)
-const SALT_ROUNDS = 12
+const SALT_ROUNDS = 12;
 
 // Función para hashear una contraseña en texto plano
-export const hashPassword = async (plain: string) : Promise<string> => {
-    return await bcrypt.hash(plain, SALT_ROUNDS)
-}
+export const hashPassword = async (plain: string): Promise<string> => {
+  return await bcrypt.hash(plain, SALT_ROUNDS);
+};
 
 // Función para comparar una contraseña en texto plano con un hash almacenado
-export const comparePassword = async (plain: string, hash: string) : Promise<boolean> => {
-    return await bcrypt.compare(plain, hash)
-} 
+export const comparePassword = async (
+  plain: string,
+  hash: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(plain, hash);
+};
