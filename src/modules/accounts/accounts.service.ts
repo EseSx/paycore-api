8640703bec8059a0d04c67cc9cc13ba51da34456
Santyyy <129233495+EseSx@users.noreply.github.com>
2026-08-07 19:29:56 -0300

    feat: User service added

---
+++ src/modules/accounts/accounts.service.ts
@@ -0,0 +1,27 @@
+import { prisma } from "../../config/prisma";
+import { createAccountInput } from "./accounts.schemas";
+
+export const createAccount = async (
+  userId: number,
+  data: createAccountInput,
+) => {
+  return prisma.account.create({
+    data: { userId, balance: data.balance ?? 0 },
+  });
+};
+
+export const getUserAccount = async (userId: number) => {
+  return prisma.account.findMany({
+    where: { userId },
+    orderBy: { id: "asc" },
+  });
+};
+
+export const getAccountById = async (userId: number, accountId: number) => {
+  const account = await prisma.account.findFirst({
+    where: { id: accountId, userId },
+  });
+
+  if (!account) throw new Error("ACCOUNT_NOT_FOUND");
+  return account;
+};
