7bbcb9fbc161c8099a66d2bdf48d9a046712840c
Santyyy <129233495+EseSx@users.noreply.github.com>
2026-08-07 18:55:36 -0300

    feat: app conected to auth

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 // Importación de dependencias necesarias para la aplicación Express
-import express from 'express'
-import helmet from 'helmet'
-import cors from 'cors'
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import authRoutes from "./modules/auth/auth.routes";
 
 // Creación de una instancia de la aplicación Express
-const app = express()
+const app = express();
 
 // Configuración de middlewares para la aplicación Express
-app.use(helmet())
-app.use(cors())
-app.use(express.json())
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 // Definición de una ruta de prueba para verificar el estado de la aplicación
-app.get('/health', (_req, res) => res.json({ status: 'ok' }))
+app.get("/health", (_req, res) => res.json({ status: "ok" }));
+
+app.use("/api/auth", authRoutes);
 
 // Exportación de la instancia de la aplicación Express para su uso en otros módulos
-export default app
+export default app;
